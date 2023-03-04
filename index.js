f075b3d5eb0f8eca29a3ff9b2210a50b0d87f18b
RealChoki <123271772+RealChoki@users.noreply.github.com>
2023-03-04 18:11:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const allCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
 const passwordLength = 12
 
 let generateButton = document.getElementById("gen-btn")
-
-
+let generatePassword1 = document.getElementById("secound-Btn")
+let generatePassword2 = document.getElementById("third-Btn")
 
 generateButton.addEventListener("click", function(){
-    function getRandomCharacter(){
-        let randomCharacter = Math.floor(Math.random()* allCharacters.length)
-        return allCharacters [randomCharacter]
-    }
-    
-    function getRandomPassword(){
-        let randomPassword = ""
-        for (let i = 0; i < passwordLength; i++){
-            randomPassword += getRandomCharacter() 
-        }
-        return randomPassword
-    }
-})
 
+function getRandomCharacter(){
+    let randomCharacter = Math.floor(Math.random()* allCharacters.length)
+    return allCharacters [randomCharacter]
+}
+    
+function getRandomPassword(){
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++){
+    randomPassword += getRandomCharacter() 
+    }
+    return randomPassword
+}
+    generatePassword1.textContent = getRandomPassword()
+    generatePassword2.textContent = getRandomPassword()
+    
+})
